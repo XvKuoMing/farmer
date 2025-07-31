15b5c08b07471db8c0793aa7a243bfeef9b42fee
XvKuoMing <kamasalyamov@gmail.com>
2025-07-31 21:40:20 +0300

    connection method

---
+++ main.py
@@ -0,0 +1,40 @@
+import asyncio
+from browser_use import Agent, BrowserSession
+from browser_use import ChatOpenAI
+# from playwright.async_api import async_playwright
+from patchright.async_api import async_playwright   # stealth alternative
+import os
+
+llm = ChatOpenAI(
+    model="gpt-4o-mini",
+    temperature=0,
+    api_key=os.getenv("OPENAI_API_KEY"),
+    base_url=os.getenv("OPENAI_BASE_URL"),
+)
+
+async def main():
+    async with async_playwright() as playwright:
+        browser = await playwright.chromium.connect_over_cdp(
+            endpoint_url="http://192.168.100.14:9222",
+        )
+        context = await browser.new_context()
+        page = await context.new_page()
+
+        browser_session = BrowserSession(
+            page=page,
+            browser_context=context,  # all these are supported
+            browser=browser,
+            playwright=playwright,
+        )
+
+        agent = Agent(
+            task="собери данные: топ 30 вопросов про ислам. Верни ответ в формате: вопрос - ответ",
+            llm=llm,
+            browser_session=browser_session,
+        )
+        history = await agent.run()
+        result = history.final_result()
+        with open("islam_questions.txt", "w", encoding="utf-8") as f:
+            f.write(result)
+
+asyncio.run(main())
